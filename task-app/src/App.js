ccfd04957de391cc8ef24aef16ba5ed1d1d2505b
nexus0208 <christiandelacruz0208@gmail.com>
2022-02-02 23:49:35 +0800

    add change, submit event

--- task-app/src/App.js
+++ task-app/src/App.js
@@ -4,11 +4,28 @@ class App extends Component {
   constructor(props) {
     super(props);
     this.state = { items: [], text: "" };
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
   }
 
+  handleChange(e) {
+    this.setState({ text: e.target.value });
+  }
 
-  
-
+  handleSubmit(e) {
+    e.preventDefault();
+    if (this.state.text.length === 0) {
+      return;
+    }
+    const newItem = {
+      text: this.state.text,
+      id: Date.now()
+    };
+    this.setState(state => ({
+      items: state.items.concat(newItem),
+      text: ''
+    }));
+  }  
 
   render() {
     return (
