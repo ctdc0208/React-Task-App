276606c1decc0fd04585d33721aa1459a7a1cb93
nexus0208 <christiandelacruz0208@gmail.com>
2022-02-02 23:21:48 +0800

    Add props

--- task-app/src/App.js
+++ task-app/src/App.js
@@ -3,29 +3,19 @@ import React, { Component } from 'react';
 class App extends Component {
   constructor(props) {
     super(props);
-    this.state = {
-        task: ["Task 1", "Task 2", "Task 3"],
-    };
-    this.onClickBtn = this.onClickBtn.bind(this);
+    this.state = { items: [], text: '' };
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
   }
 
-  onClearArray = () => {
-    this.setState({ task: [] });
-    console.log('Clear Array!');
-  };
 
-  onClickBtn() {
-    console.log('Button has been clicked!');
-  }
+  
 
 
   render() {
     return (
       <div>
         <h1>Task List</h1>
-        <button onClick={this.onClickBtn}>Click Me!</button>
-
-        <button type="button" onClick={this.onClearArray}>Clear Array</button>
       </div>
     );
   }
